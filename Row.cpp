3d0a025f398fd14448587138d677adf3af8f1c8c
David Williams <xpositivityx@gmail.com>
2016-06-12 15:27:16 -0700

    added data structure to handle multiple pixles. compiled with no errors

--- Row.cpp
+++ Row.cpp
@@ -8,7 +8,12 @@ Row::Row(int rowOffset, bool reverse, Adafruit_NeoPixel& pixels)
 	_rowOffset = rowOffset;
 	_reverse = reverse;
   _pixels = pixels;
-  _firstPixel = new Pixel(_rowOffset);
+  Pixel *pixel = new Pixel(_rowOffset);
+  _pixelList = new LinkedList<Pixel*>;
+  _pixelList->add(pixel);
+  _onColor = _pixels.Color(25,25,25);
+  _offColor = _pixels.Color(0,0,0);
+  _brightColor = _pixels.Color(50,50,50);
   setIncrementor();
 }
 
@@ -17,25 +22,22 @@ void Row::turnPixelOn(int pixelNum)
 	if(_reverse){
 		pixelNum = (_rowOffset) + (_rowOffset - pixelNum) - 1;
 	}
-  Serial.print(_rowOffset);
-  Serial.print(pixelNum);
-  // make this call reusable
+
   if(pixelNum == _rowOffset - 1 && _reverse){
-   _pixels.setPixelColor(_rowOffset - ROWLENGTH, _pixels.Color(0,0,0));
+   _pixels.setPixelColor(_rowOffset - ROWLENGTH, _offColor);
    Serial.print(pixelNum);//hide the last one
   }
 
   if(pixelNum == _rowOffset)
   {
-    _pixels.setPixelColor(_rowOffset +ROWLENGTH - 1, _pixels.Color(0,0,0));
+    _pixels.setPixelColor(_rowOffset + ROWLENGTH - 1, _offColor);
   }
   // fix the incrementor so it wont be static.
-  _pixels.setPixelColor(pixelNum - _incrementor, _pixels.Color(0,0,0));//hide the last one
-  _pixels.setPixelColor(pixelNum, _pixels.Color(25,25,25));//show the current one
+  _pixels.setPixelColor(pixelNum - _incrementor, _offColor);//hide the last one
+  _pixels.setPixelColor(pixelNum, _onColor);//show the current one
   _pixels.show();
 }
 
-//This function is completely non functional.
 void Row::setIncrementor()
 {
   if(_reverse){
@@ -44,11 +46,22 @@ void Row::setIncrementor()
     _incrementor = 1;
   }
 }
+
+void Row::createPixel()
+{
+
+  Pixel *newPixel = new Pixel(_rowOffset);
+  _pixelList->add(newPixel);
+}
+
 // make void for final version. Int just for debugging.
 int Row::handlePixel()
 {
-	if(_firstPixel->checkTime()){
-		turnPixelOn(_firstPixel->position);
-	}
- return _firstPixel->position;
+ int size = _pixelList->size();
+ for(int i = 0; i < size - 1; i++){
+  if(_pixelList->get(i)->checkTime()){
+    turnPixelOn(_pixelList->get(i)->position);
+  }
+ }
+ return 1;
 }
--- Row.h
+++ Row.h
@@ -4,6 +4,7 @@
 #include "Pixel.h"
 #include "Arduino.h"
 #include <Adafruit_NeoPixel.h>
+#include <LinkedList.h>
 
 class Row
 {
@@ -11,14 +12,17 @@ class Row
 		Row(int rowOffset, bool reverse, Adafruit_NeoPixel& pixels);
 		int handlePixel();
 	private:
-		Pixel* _firstPixel;
 		Adafruit_NeoPixel _pixels;
 		int _incrementor;
 		int _rowOffset;
+		long _onColor;
+		long _offColor;
+		long _brightColor;
 		bool _reverse;
     void turnPixelOn(int pixelNum);
     void setIncrementor();
     void createPixel();
+    LinkedList<Pixel*>* _pixelList;
 
 };
 
